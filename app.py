96ccc784de2ac3670a5922c0e319ae981db084e2
nyaivu <marvtfs@gmail.com>
2026-03-28 17:43:16 +0800

    Added Populations and Map Endpoints

--- app.py
+++ app.py
@@ -50,5 +50,113 @@ def get_stats():
         "mean_ndvi": ndvi_value
     })
 
+@app.route("/map/<city>")
+def getCityMap(city):
+    isKabupaten = request.args.get("kabupaten")
+
+    cityWithSpace = city.replace('-', ' ')
+
+    if isKabupaten:
+        cityString = str.title(cityWithSpace)
+    else:
+        cityString = f'Kota {str.title(cityWithSpace)}'
+
+    population = ee.Image("JRC/GHSL/P2023A/GHS_POP/1975")
+    indoCities = ee.FeatureCollection("FAO/GAUL/2015/level2").filter(ee.Filter.eq('ADM2_NAME', cityString))
+
+    if indoCities.size().getInfo() == 0:
+        return jsonify({
+            "error": f"City '{cityString}' not found in the GAUL database.",
+            "suggestion": "Check if it should be a 'kabupaten' or if the spelling matches 'Kota ...'"
+        }), 404
+
+    targetCity = indoCities.first()
+
+    if targetCity is None:
+        return jsonify({"error": f"City {cityString} not found"}), 404
+
+    centroid = targetCity.geometry().centroid()
+    coords = centroid.getInfo()['coordinates']
+    zoom_level = 11
+
+    vis_params = {
+        'min': 0,
+        'max': 100,
+        'palette': ['f7f7f7', 'cccccc', '969696', '525252', '000000']
+    }
+
+    masked_population = population.updateMask(population.gt(0))
+
+    map_data = masked_population.getMapId(vis_params)
+
+    return jsonify({
+        "city": cityString,
+        "lat": coords[1],
+        "lng": coords[0],
+        'tile_url': map_data['tile_fetcher'].url_format
+    })
+
+@app.route("/population/<city>")
+def getCityPopulation(city):
+    isKabupaten = request.args.get("kabupaten")
+
+    cityWithSpace = city.replace('-', ' ')
+
+    if isKabupaten:
+        cityString = str.title(cityWithSpace)
+    else:
+        cityString = f'Kota {str.title(cityWithSpace)}'
+
+    population = ee.Image("JRC/GHSL/P2023A/GHS_POP/1975")
+    indoCities = ee.FeatureCollection("FAO/GAUL/2015/level2").filter(ee.Filter.eq('ADM2_NAME', cityString))
+
+    populationStats = population.reduceRegions(
+        collection = indoCities, 
+        reducer = ee.Reducer.sum(), 
+        scale = 100
+    )
+    cityStats = populationStats.select(['ADM2_NAME', 'sum'], retainGeometry=False).getInfo()
+    
+    features = cityStats.get('features', [])
+    currentFeature = features[0]
+
+    # 2. Use a list comprehension to extract only what you need
+    clean_list = {
+        "data": {
+            "id": currentFeature.get("id"),
+            "city": currentFeature["properties"].get("ADM2_NAME"),
+            "population": round(currentFeature["properties"].get("sum", 0))
+        }
+    } 
+
+    return jsonify(clean_list)
+
+@app.route("/population/all")
+def population():
+    population = ee.Image("JRC/GHSL/P2023A/GHS_POP/2020")
+    indoCities = ee.FeatureCollection("FAO/GAUL/2015/level2").filter(ee.Filter.eq('ADM0_NAME', 'Indonesia'))
+
+    populationStats = population.reduceRegions(
+        collection = indoCities, 
+        reducer = ee.Reducer.sum(), 
+        scale = 100
+    )
+    cityStats = populationStats.select(['ADM2_NAME', 'sum'], retainGeometry=False).getInfo()
+    features = cityStats.get('features', [])
+
+    # 2. Use a list comprehension to extract only what you need
+    clean_list = {
+        "data": [
+            {
+            "id": f.get("id"),
+            "city": f["properties"].get("ADM2_NAME"),
+            "population": round(f["properties"].get("sum", 0))
+        }
+        for f in features
+        ]
+    } 
+
+    return jsonify(clean_list)
+
 if __name__ == '__main__':
     app.run(debug=True)
